4de74a745aafc4395ae13b2a462a27f17dd90cc9
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-09-18 13:00:42 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir container
+mkdir container/drain3
+mkdir container/drain3/results
+mkdir container/drain3/results/frontend
+mkdir container/drain3/results/backend
+mkdir container/DeepLog
+mkdir containet/DeepLog/results
