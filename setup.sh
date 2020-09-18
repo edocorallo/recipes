b1afd5af636d0bade17bd74d1bf7a694ab91b8e1
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-09-18 13:02:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 mkdir container
 mkdir container/drain3
 mkdir container/drain3/results
 mkdir container/drain3/results/frontend
 mkdir container/drain3/results/backend
 mkdir container/DeepLog
-mkdir containet/DeepLog/results
+mkdir container/DeepLog/results
