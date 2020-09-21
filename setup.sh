c890ec61dbad742037c9a61ba79d87fae8b58978
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-09-21 13:11:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+read -e -p "Please enter path to logfile directory (starting from this very directory):" -r log_path
+echo "Setting SINGULARITY_BINDPATH in .bashrc ..."
+echo 'export SINGULARITY_BINDPATH = "'$log_path':/container/logfiles/,container/drain3/results/:/container/drain3/parser/results/,container/DeepLog/model/:/container/DeepLog_no_tensorboard/model/"'>> ../.bashrc
+echo "Setting up directories for binding ..."
 mkdir container
 mkdir container/drain3
 mkdir container/drain3/results
@@ -6,3 +10,4 @@ mkdir container/drain3/results/frontend-server
 mkdir container/drain3/results/backend-server
 mkdir container/DeepLog
 mkdir container/DeepLog/model
+echo "Done."
